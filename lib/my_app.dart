ed9847ed77233e7e5997d7720795c391a40f3c6c
EgorEko <asya.wear@gmail.com>
2022-05-23 07:08:43 +0300

    Added navigation service and extracted routes

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:study/app_injector.dart';
-import 'package:study/app_routes_names.dart';
-import 'package:study/pages/home/home_page_view_model.dart';
-import 'package:study/pages/issue_details/issue_details_page.dart';
-import 'package:study/pages/issues/issues_page_build.dart';
-import 'package:study/services/increment_service.dart';
-import 'package:study/services/navigation_service.dart';
-import 'package:study/title_service.dart';
 
 import 'app_routes.dart';
-import 'pages/home/home_page.dart';
 
 class MyApp extends StatelessWidget {
   final String initialRoute;
